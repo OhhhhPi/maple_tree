09d3ab7c5cdf65146c3ef41ad4095cb4db92d293
Chil <liangqao@mail2.sysu.edu.com>
2023-11-17 09:04:20 +0800

    update

--- maple_tree.h
+++ maple_tree.h
@@ -1,42 +1,152 @@
 #include <pthread.h>
-#include"spinlock.h"
+#include "spinlock.h"
 #include <urcu.h>
+#include <limits.h>
 
 // macro
-#define MA_STATE(name, mt, first, end)					\
-	struct ma_state name = {					\
-		.tree = mt,						\
-		.index = first,						\
-		.last = end,						\
-		.node = MAS_START,					\
-		.min = 0,						\
-		.max = ULONG_MAX,					\
-		.alloc = NULL,						\
-		.mas_flags = 0,						\
+
+#define MAS_START ((struct maple_enode *)1UL) // we have not searched the tree
+#define MAS_ROOT             \
+	((struct maple_enode \
+		  *)5UL) // we have searched the tree and the entry we found lives in the root of the tree
+#define MAS_NONE             \
+	((struct maple_enode \
+		  *)9UL) // we have searched the tree and there is no node in the tree for this entry
+#define MAS_PAUSE ((struct maple_enode *)17UL) //
+#define MAS_OVERFLOW ((struct maple_enode *)33UL)
+#define MAS_UNDERFLOW ((struct maple_enode *)65UL)
+// MA_ERROR represents an errno.
+// After dropping the lock and attempting to resolve the error,
+// the walk would have to be restarted from the top of the tree as the tree may have been modified.
+#define MA_ERROR(err) ((struct maple_enode *)(((unsigned long)err << 2) | 2UL))
+
+#define MA_STATE(name, mt, first, end) \
+	struct ma_state name = {       \
+		.tree = mt,            \
+		.index = first,        \
+		.last = end,           \
+		.node = MAS_START,     \
+		.min = 0,              \
+		.max = ULONG_MAX,      \
+		.alloc = NULL,         \
+		.mas_flags = 0,        \
 	}
 
-//data structures 
+#define MA_WR_STATE(name, ma_state, wr_entry) \
+	struct ma_wr_state name = {           \
+		.mas = ma_state,              \
+		.content = NULL,              \
+		.entry = wr_entry,            \
+	}
+
+/*
+ * Allocated nodes are mutable until they have been inserted into the tree,
+ * at which time they cannot change their type until they have been removed
+ * from the tree and an RCU grace period has passed.
+ *
+ * Removed nodes have their ->parent set to point to themselves.  RCU readers
+ * check ->parent before relying on the value that they loaded from the
+ * slots array.  This lets us reuse the slots array for the RCU head.
+ *
+ * Nodes in the tree point to their parent unless bit 0 is set.
+ */
+
+/* 64bit sizes */
+#define MAPLE_NODE_SLOTS 31 /* 256 bytes including ->parent */
+#define MAPLE_RANGE64_SLOTS 16 /* 256 bytes */
+#define MAPLE_ARANGE64_SLOTS 10 /* 240 bytes */
+#define MAPLE_ARANGE64_META_MAX 15 /* Out of range for metadata */
+#define MAPLE_ALLOC_SLOTS (MAPLE_NODE_SLOTS - 1)
+
+//data structures
 struct maple_tree {
 	union {
 		// locks
 		spinlock ma_lock;
 	};
-	void       *ma_root;
-	unsigned int	ma_flags;
-
+	void *ma_root;
+	unsigned int ma_flags;
 };
 
 struct ma_state {
-	struct maple_tree *tree;	/* The tree we're operating in */
-	unsigned long index;		/* The index we're operating on - range start */
-	unsigned long last;		/* The last index we're operating on - range end */
-	struct maple_enode *node;	/* The node containing this entry */
-	unsigned long min;		/* The minimum index of this node - implied pivot min */
-	unsigned long max;		/* The maximum index of this node - implied pivot max */
-	struct maple_alloc *alloc;	/* Allocated nodes for this operation */
-	unsigned char depth;		/* depth of tree descent during write */
+	struct maple_tree *tree; /* The tree we're operating in */
+	unsigned long index; /* The index we're operating on - range start */
+	unsigned long last; /* The last index we're operating on - range end */
+	struct maple_enode *node; /* The node containing this entry */
+	unsigned long
+		min; /* The minimum index of this node - implied pivot min */
+	unsigned long
+		max; /* The maximum index of this node - implied pivot max */
+	struct maple_alloc *alloc; /* Allocated nodes for this operation */
+	unsigned char depth; /* depth of tree descent during write */
 	unsigned char offset;
 	unsigned char mas_flags;
+};
+
+/*
+ * This metadata is used to optimize the gap updating code and in reverse
+ * searching for gaps or any other code that needs to find the end of the data.
+ */
+struct maple_metadata {
+	unsigned char end;
+	unsigned char gap;
+};
+
+/*
+ * Leaf nodes do not store pointers to nodes, they store user data.  Users may
+ * store almost any bit pattern.  As noted above, the optimisation of storing an
+ * entry at 0 in the root pointer cannot be done for data which have the bottom
+ * two bits set to '10'.  We also reserve values with the bottom two bits set to
+ * '10' which are below 4096 (ie 2, 6, 10 .. 4094) for internal use.  Some APIs
+ * return errnos as a negative errno shifted right by two bits and the bottom
+ * two bits set to '10', and while choosing to store these values in the array
+ * is not an error, it may lead to confusion if you're testing for an error with
+ * mas_is_err().
+ *
+ * Non-leaf nodes store the type of the node pointed to (enum maple_type in bits
+ * 3-6), bit 2 is reserved.  That leaves bits 0-1 unused for now.
+ *
+ * In regular B-Tree terms, pivots are called keys.  The term pivot is used to
+ * indicate that the tree is specifying ranges,  Pivots may appear in the
+ * subtree with an entry attached to the value whereas keys are unique to a
+ * specific position of a B-tree.  Pivot values are inclusive of the slot with
+ * the same index.
+ */
+
+struct maple_range_64 {
+	struct maple_pnode *parent;
+	unsigned long pivot[MAPLE_RANGE64_SLOTS - 1];
+	union {
+		void __rcu *slot[MAPLE_RANGE64_SLOTS];
+		struct {
+			void __rcu *pad[MAPLE_RANGE64_SLOTS - 1];
+			struct maple_metadata meta;
+		};
+	};
+};
+
+/*
+ * At tree creation time, the user can specify that they're willing to trade off
+ * storing fewer entries in a tree in return for storing more information in
+ * each node.
+ *
+ * The maple tree supports recording the largest range of NULL entries available
+ * in this node, also called gaps.  This optimises the tree for allocating a
+ * range.
+ */
+struct maple_arange_64 {
+	struct maple_pnode *parent;
+	unsigned long pivot[MAPLE_ARANGE64_SLOTS - 1];
+	void __rcu *slot[MAPLE_ARANGE64_SLOTS];
+	unsigned long gap[MAPLE_ARANGE64_SLOTS];
+	struct maple_metadata meta;
+};
+
+struct maple_alloc {
+	unsigned long total;
+	unsigned char node_count;
+	unsigned int request_count;
+	struct maple_alloc *slot[MAPLE_ALLOC_SLOTS];
 };
 
 /*
@@ -54,7 +164,12 @@ struct ma_state {
  * 4. Leaf Range nodes All nodes consist of a number of node slots,
  *    pivots, and a parent pointer.
  */
-
+enum maple_type {
+	maple_dense,
+	maple_leaf_64,
+	maple_range_64,
+	maple_arange_64,
+};
 struct maple_node {
 	union {
 		struct {
@@ -75,7 +190,20 @@ struct maple_node {
 		struct maple_alloc alloc;
 	};
 };
-
+struct ma_wr_state {
+	struct ma_state *mas;
+	struct maple_node *node; /* Decoded mas->node */
+	unsigned long r_min; /* range min */
+	unsigned long r_max; /* range max */
+	enum maple_type type; /* mas->node type */
+	unsigned char offset_end; /* The offset where the write ends */
+	unsigned char node_end; /* mas->node end */
+	unsigned long *pivots; /* mas->node->pivots pointer */
+	unsigned long end_piv; /* The pivot at the offset end */
+	void __rcu **slots; /* mas->node->slots pointer */
+	void *entry; /* The entry to write */
+	void *content; /* The existing entry that is being overwritten */
+};
 
 /*
 Start by initialising a maple tree
@@ -116,19 +244,15 @@ If the maple tree entries are pointers, free the entries first.
 */
 
 // Normal API
-#define DEFINE_MTREE(name)						\
-	struct maple_tree name = MTREE_INIT(name, 0)
+#define DEFINE_MTREE(name) struct maple_tree name = MTREE_INIT(name, 0)
 
-#define MTREE_INIT(name, __flags) {					\
-	.ma_lock = SPINLOCK_INITIALIZER,		\
-	.ma_flags = __flags,						\
-	.ma_root = NULL,						\
-}
+#define MTREE_INIT(name, __flags)                                     \
+	{                                                             \
+		.ma_lock = SPINLOCK_INITIALIZER, .ma_flags = __flags, \
+		.ma_root = NULL,                                      \
+	}
 static inline void mt_init_flags(struct maple_tree *mt, unsigned int flags);
 static inline void mt_init(struct maple_tree *mt);
-
-
-
 
 // Takes RCU read lock:
 //  * mtree_load()
@@ -139,12 +263,13 @@ static inline void mt_init(struct maple_tree *mt);
 
 void *mtree_load(struct maple_tree *mt, unsigned long index);
 void *mt_find(struct maple_tree *mt, unsigned long *index, unsigned long max);
-#define mt_for_each(__tree, __entry, __index, __max) \
-	for (__entry = mt_find(__tree, &(__index), __max); \
-		__entry; __entry = mt_find_after(__tree, &(__index), __max))
-void *mt_find_after(struct maple_tree *mt, unsigned long *index, unsigned long max);
+#define mt_for_each(__tree, __entry, __index, __max)                \
+	for (__entry = mt_find(__tree, &(__index), __max); __entry; \
+	     __entry = mt_find_after(__tree, &(__index), __max))
+void *mt_find_after(struct maple_tree *mt, unsigned long *index,
+		    unsigned long max);
 void *mt_next(struct maple_tree *mt, unsigned long index, unsigned long max);
-void *mt_prev(struct maple_tree *mt, unsigned long index, unsigned long min);	
+void *mt_prev(struct maple_tree *mt, unsigned long index, unsigned long min);
 
 // Takes ma_lock internally:
 //  * mtree_store()
@@ -156,11 +281,17 @@ void *mt_prev(struct maple_tree *mt, unsigned long index, unsigned long min);
 //  * mt_set_in_rcu()
 //  * mt_clear_in_rcu()
 
-int mtree_store_range(struct maple_tree *mt, unsigned long first, unsigned long last, void *entry);
-int mtree_store(struct maple_tree *mt, unsigned long index,	void *entry);
+int mtree_store_range(struct maple_tree *mt, unsigned long first,
+		      unsigned long last, void *entry);
+int mtree_store(struct maple_tree *mt, unsigned long index, void *entry);
 int mtree_insert(struct maple_tree *mt, unsigned long index, void *entry);
-int mtree_insert_range(struct maple_tree *mt, unsigned long first, unsigned long last, void *entry);
+int mtree_insert_range(struct maple_tree *mt, unsigned long first,
+		       unsigned long last, void *entry);
 void *mtree_erase(struct maple_tree *mt, unsigned long index);
 void mtree_destroy(struct maple_tree *mt);
-static inline void mt_set_in_rcu(struct maple_tree *mt){}
-static inline void mt_clear_in_rcu(struct maple_tree *mt){}
+static inline void mt_set_in_rcu(struct maple_tree *mt)
+{
+}
+static inline void mt_clear_in_rcu(struct maple_tree *mt)
+{
+}
